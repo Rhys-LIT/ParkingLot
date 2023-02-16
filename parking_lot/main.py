206a8fa8151fb77685517057ea7103a8c9cb11b7
RhysQuilter <98286839+Rhys-LIT@users.noreply.github.com>
2023-02-16 12:27:52 +0000

    json fix

--- parking_lot/main.py
+++ parking_lot/main.py
@@ -21,7 +21,7 @@ def main():
             generator.generate()
 
     with open(data_file, "r") as data:
-        points = yaml.load(data)
+        points = yaml.full_load(data)
         detector = MotionDetector(args.video_file, points, int(start_frame))
         detector.detect_motion()
 
